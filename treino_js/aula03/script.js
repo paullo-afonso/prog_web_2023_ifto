6fa15516fa164e78f668cdffb5968c0abec75cd6
Paulo Afonso <contato@pauloafonso.dev>
2023-09-09 23:29:25 -0300

    limpando pasta raiz do curso

--- treino_js/aula03/script.js
+++
@@ -1,7 +0,0 @@
-//input no javascript
-const user = prompt('Qual é o seu nome?')
-const senha = prompt('Qual é sua senha?')
-
-//output
-console.log('Usuário ',user,' Logado!')
-alert(user)
--- treino_js/aula13/script.js
+++
@@ -1,7 +0,0 @@
-// Operador Ternário
-
-let admin = 35;
-// condição?valor_verdadeiro:valor_falso;
-let label = (admin>15) ? 'Administrador':'Comum';
-
-alert(`Paulo é um ${label}`);
