b8b4d3c8f1ad909dfa74558b06d46968c4599c94
Paulo Afonso <paulo@pauloafonso.dev>
2023-10-02 23:25:40 -0300

    add files from aula 29

---
+++ treino_js/aula29/script.js
@@ -0,0 +1,22 @@
+// function soma(x, y) {
+//     console.log(x+y)
+// }
+
+
+//Função Anônima
+// const soma = function (x, y) {
+//     console.log(x+y)
+// }
+
+
+// soma = (x,y) => console.log(x ** 2 + y);
+
+
+//arrow function
+soma = (x , y) => {
+    x++;
+    console.log(x ** 2 + y);
+}
+
+soma(5,2);
+
