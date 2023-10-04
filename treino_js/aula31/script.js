bb4d828c41d5b25e9d20c5033975f9f0609e9d5f
Paulo Afonso <paulo@pauloafonso.dev>
2023-10-03 23:53:08 -0300

    add files class 31

---
+++ treino_js/aula31/script.js
@@ -0,0 +1,18 @@
+function mudarCor(element, cor = ''){
+    element.style.backgroundColor = cor;
+}
+
+function block(block = true) {
+    const cx = document.getElementsByClassName('block')[0];
+    cx.style.display = (block)?'block' : 'none';
+}
+
+function mover(event){
+    // console.log(event);
+    const cord = document.getElementById('cord');
+    cord.textContent = `${event.x},${event.y}`;
+    const bola = document.querySelector('.bola');
+    // console.log(event)
+    bola.style.top = (event.y-100)+'px';
+    bola.style.left = (event.x-100)+'px';
+}
